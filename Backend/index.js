f5dbd1e007d79903d8351858919d344bd002167f
Soubhik <soubhikpramanik10@gmail.com>
2025-04-30 15:22:28 +0530

    Implement signup, login, logout, profile update, and auth check routes with controllers

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
 import dotenv from "dotenv";
 import connectDB from "./src/lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,16 +1,14 @@
+import cloudinary from "../lib/cloudinary.js";
 import generateToken from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
-import validator from "validator"
+import validator from "validator";
 
 const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    
-    if (!fullName || !email || !password ) {
-      return res
-        .status(400)
-        .json({ message: "All fields are required" });
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     //Email validation
@@ -19,7 +17,7 @@ const signup = async (req, res) => {
         .status(400)
         .json({ message: "You have entered wrong email id" });
     }
-    
+
     //Password validation
     if (password.length < 6) {
       return res
@@ -61,11 +59,75 @@ const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
-const login = (req, res) => {
-  res.send("Signup route");
-};
-const logout = (req, res) => {
-  res.send("Signup route");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    generateToken(user._id, res);
+
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log(`Error in login controller, ${error.message}`);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export { signup, login, logout };
+const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    return res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log(`Error in logout controller, ${error.message}`);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(`Error in update profile, ${error.message}`);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const checkAuth = (req,res) => {
+  try {
+    return res.status(200).json(req.user);
+  } catch (error) {
+    console.log(`Error in checkAuth controller, ${error.message}`);
+    res.status(500).json({message : "Internal server error"})
+  }
+};
+
+export { signup, login, logout, updateProfile, checkAuth };
