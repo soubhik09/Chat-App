13f5c6e40c0b87404439af5c44edde3ad4328c82
Soubhik <soubhikpramanik10@gmail.com>
2025-05-01 16:59:33 +0530

    Solve cors error

--- Backend/index.js
+++ Backend/index.js
@@ -10,13 +10,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
