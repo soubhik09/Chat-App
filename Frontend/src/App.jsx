9c6fb5ad2bdf56ac5f5a4fe1682b5fa5d6ded133
Soubhik <soubhikpramanik10@gmail.com>
2025-05-01 14:26:25 +0530

    Implement signup & login pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import SignupPage from "./pages/SignupPage";
+import useAuthStore from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-15 animate-spin" />
+      </div>
+    );
+  }
+
   return (
-    <div className='bg-red-200 text-center'>App</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import useAuthStore from "../store/useAuthStore";
+import AuthImagePattern from "../components/AuthImagePattern";
+import { Link } from "react-router";
+import {
+  Eye,
+  EyeOff,
+  Loader2,
+  Lock,
+  Mail,
+  MessageSquare,
+} from "lucide-react";
+
+function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [fromData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(fromData);
+  };
+
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8 ">
+          {/* logo */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+              <p className="text-base-content/60">
+                Sign in to your account
+              </p>
+            </div>
+          </div>
+
+          {/* Form */}
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
+              <label className="lable">
+                <span className="lebel-text font-medium">Email</span>
+              </label>
+              <div className="relative mt-1">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="z-10 size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input focus:input-primary focus:outline-none w-full pl-10`}
+                  placeholder="John@example.com"
+                  value={fromData.email}
+                  onChange={(e) =>
+                    setFromData({ ...fromData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="lable">
+                <span className="lebel-text font-medium">Password</span>
+              </label>
+              <div className="relative mt-1">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="z-10 size-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={`input focus:input-primary focus:outline-none w-full pl-10 `}
+                  placeholder="● ● ● ● ● ●"
+                  value={fromData.password}
+                  onChange={(e) =>
+                    setFromData({ ...fromData, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="z-10 absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isLoggingIn}
+            >
+              {isLoggingIn ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Sign in"
+              )}
+            </button>
+          </form>
+          <div className="text-center">
+            <p className="text-base-content/60">
+              {`Don't have an account? `}
+              <Link to="/signup" className="link link-primary">
+                Create account
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* right side */}
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
+    </div>
+  );
+}
+
+export default LoginPage;
