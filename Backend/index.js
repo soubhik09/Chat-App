19280ec15845c8397198c5a7a6f0a0a98ea685b6
Soubhik <soubhikpramanik10@gmail.com>
2025-05-01 14:29:37 +0530

    Install CORS & Config it

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./src/routes/message.route.js";
@@ -10,7 +11,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
