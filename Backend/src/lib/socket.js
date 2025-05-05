ddd2c60eab5c8c0eef1921d3827b43808e4aa3ad
Soubhik <soubhikpramanik10@gmail.com>
2025-05-05 20:25:44 +0530

    Add last commit

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -22,7 +22,9 @@ io.on("connection", (socket) => {
   console.log("A user connected ", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId && userId.trim() !== "") {
+    userSocketMap[userId] = socket.id;
+  }
 
   //io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
